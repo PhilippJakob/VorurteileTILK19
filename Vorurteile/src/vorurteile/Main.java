eb699325f67e5f472279c995d8fc6bc8a31ba45c
Jonas N. Henle <Jonas N. Henle@DESKTOP-SNSV>
2018-01-18 13:09:36 +0100

    Programmtitel u. Programmkopf erstellt.

--- Vorurteile/src/vorurteile/Main.java
+++ Vorurteile/src/vorurteile/Main.java
@@ -1,26 +1,63 @@
+//  ___________________________________________________________________________________________________________________
+//  | ####################################### |      Name Main.java          | ###################################### |
+//  | ####################################### | Origin = Philipp Jakob V 0.1 | ###################################### |
+//  | ####################################### |   @Author = Jonas N. Henle   | ###################################### |
+//  | ####################################### |        Version 0.1.1         | ###################################### |
+//  | ####################################### |     Date: 18.01.18   12:58   | ###################################### |
+//  | ####################################### |Group Vorurteile [Ben / Ohan] | ###################################### |
+//  |_________________________________________|______________________________|________________________________________|
+
 package vorurteile;
-	
+
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 
-
 public class Main extends Application {
+
+	private Stage bühne;
+	private AnchorPane pane;
+
 	@Override
-	public void start(Stage primaryStage) {
+	public void start(Stage pPrimaryStage) {
+
+		this.setBühne(pPrimaryStage);
+		this.getBühne().setTitle("Vorurteile hinzufügen");
+
 		try {
 			BorderPane root = new BorderPane();
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
+			pPrimaryStage.setScene(scene);
+			pPrimaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
+	}
+
+	public Stage getBühne()
+	{
+		return bühne;
+	}
+
+	public AnchorPane getPane()
+	{
+		return pane;
+	}
+
+	public void setBühne(Stage pBühne)
+	{
+		bühne = pBühne;
+	}
+
+	public void setPane(AnchorPane pPane)
+	{
+		pane = pPane;
 	}
 }
