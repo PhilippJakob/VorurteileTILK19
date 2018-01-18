fa1d5969c7ccc08282ca4bcb2d7fc06697308aaa
schoebel.maximilian <schoebel.maximilian@PC232621.osz.intern>
2018-01-18 11:42:34 +0100

    Design von der Hinzufügenfunktion der Quellen

--- Vorurteile/src/fakten/EingabeMain.java
+++ Vorurteile/src/fakten/EingabeMain.java
@@ -1,18 +1,15 @@
-package vorurteile;
+package fakten;
 
 import javafx.application.Application;
-import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
-
-public class Main extends Application {
+public class EingabeMain extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			Scene scene = new Scene(FXMLLoader.load(getClass().getResource("EingabeFaktView.fxml")));
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
