1b74d706c4b4a413551bf1549015119fcbcb6c13
neubert.tobias <neubert.tobias@PC230111.osz.intern>
2018-01-24 11:49:22 +0100

    Oberflaeche und Controller erstellt

---
+++ Vorurteile/src/hierarchien/AnzeigeVorurteilsHierarchieController.java
@@ -0,0 +1,23 @@
+/**
+ * bearbeitet von Gracjan Flik und Tobias Neubert am 22.01.2018
+ */
+
+package hierarchien;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class AnzeigeVorurteilsHierarchieController
+{
+
+    @FXML
+    private TextField tfVorurteil1, tfVorurteil2, tfVorurteil3;
+
+    @FXML
+    private TextField tfVorVorurteil1, tfVorVorurteil2, tfVorVorurteil3;
+
+    @FXML
+    private TextField tfFakt1, tfFakt2, tfFakt3, tfFakt4, tfFakt5,
+    						 tfFakt6, tfFakt7, tfFakt8, tfFakt9;
+
+}//end
