6e5b2a6533c00ba384beda8e31a350554f9cd212
HenderkesFlorian <f.henderkes@outlook.de>
2018-01-24 11:41:23 +0000

    Update Datenbankverbindung.java

--- Vorurteile/src/datenbank/Datenbankverbindung.java
+++ Vorurteile/src/datenbank/Datenbankverbindung.java
@@ -5,19 +5,12 @@ import java.sql.*;
 public class Datenbankverbindung
 {
 	private static Connection connection;
-	private static String host = "dbserver";
-	private static String database = "dbo_vorurteile";
-	private static String user = "dbuser";
-	private static String password = "benutzer";
+	public static String host = "dbserver";
+	public static String database = "dbo_vorurteile";
+	public static String user = "dbuser";
+	public static String password = "benutzer";
 
-	public static void setzenParameter(String pHost, String pDatabase, String pUser, String pPassword)
-	{
-		host = pHost;
-		database = pDatabase;
-		user = pUser;
-		password = pPassword;
-	}
-	public static Connection herstellenVerbindung()
+	public static Connection getConnection()
 	{
 		if(connection == null)
 		{
