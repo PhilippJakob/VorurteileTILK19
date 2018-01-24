860aa2f6e2e34f5cb16da87df0fe17907198200a
neubert.tobias <neubert.tobias@PC230111.osz.intern>
2018-01-24 12:42:20 +0100

    Testklasse hinzugefügt

--- Vorurteile/src/hierarchien/AnzeigeVorurteilsHierarchieController.java
+++ Vorurteile/src/hierarchien/AnzeigeVorurteilsHierarchieController.java
@@ -10,6 +10,8 @@ import javafx.scene.control.TextField;
 public class AnzeigeVorurteilsHierarchieController
 {
 
+	Testklasse tk;
+
     @FXML
     private TextField tfVorurteil1, tfVorurteil2, tfVorurteil3;
 
@@ -19,5 +21,29 @@ public class AnzeigeVorurteilsHierarchieController
     @FXML
     private TextField tfFakt1, tfFakt2, tfFakt3, tfFakt4, tfFakt5,
     						 tfFakt6, tfFakt7, tfFakt8, tfFakt9;
+//
+//    public void test()
+//    {
+//   	 tk = new Testklasse("Alle Bäume sind braun");
+//   	 tk.hinzufuegenFakt("Fakt 1");
+//   	 tk.hinzufuegenFakt("Fakt 2");
+//   	 tk.hinzufuegenFakt("Fakt 3");
+//
+//   	 tk.hinzufuegenVorVorurteil("Stimmt nicht");
+//   	 tk.hinzufuegenVorVorurteil("Es gibt auch weiße");
+//   	 tk.hinzufuegenVorVorurteil("Was, Äpfel, Eier, Plaumen?");
+//
+//   	 tfVorurteil1.setText(tk.getVorurteil());
+//
+//   	 tfFakt1.setText(tk.getFakten().get(0));
+//   	 tfFakt2.setText(tk.getFakten().get(1));
+//   	 tfFakt3.setText(tk.getFakten().get(2));
+//
+//   	 tfVorVorurteil1.setText(tk.getVorVorurteile().get(0));
+//   	 tfVorVorurteil2.setText(tk.getVorVorurteile().get(1));
+//   	 tfVorVorurteil3.setText(tk.getVorVorurteile().get(2));
+//    }
+
+
 
 }//end
---
+++ Vorurteile/src/hierarchien/Main.java
@@ -0,0 +1,40 @@
+/**
+ *  bearbeitet von Gracjan Flik und Tobias Neubert am 22.01.2018
+ */
+package hierarchien;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+
+
+public class Main extends Application
+{
+	@Override
+	public void start(Stage primaryStage)
+	{
+		try
+		{
+			AnzeigeVorurteilsHierarchieController controller = new AnzeigeVorurteilsHierarchieController();
+			FXMLLoader lLoader = new FXMLLoader(getClass().getResource("AnzeigeVoruteilsHierarchieView.fxml"));
+			lLoader.setController(controller);
+			AnchorPane lPane = lLoader.load();
+			Scene lSzene = new Scene(lPane);
+			primaryStage.setScene(lSzene);
+			primaryStage.show();
+//			controller.test();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args)
+	{
+		launch(args);
+	}
+
+}//end
