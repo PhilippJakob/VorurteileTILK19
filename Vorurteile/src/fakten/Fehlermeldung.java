337be9935126778cd904f432d14c3d0f5e927712
shahmirzadi.amir_sas <shahmirzadi.amir_sas@PC232624.osz.intern>
2018-01-24 13:54:11 +0100

    Feld/er nicht hinzugefügt bei Ideen bearbeitet

--- Vorurteile/src/fakten/Fehlermeldung.java
+++ Vorurteile/src/fakten/Fehlermeldung.java
@@ -33,7 +33,10 @@ package fakten;
 String w = "atgaagtctt";
 int anzahl = 0;
 for (int i = 0; i < w.length(); i++) {
-   if (w.charAt(i) == 'a' && 'b' && 'c' && 'd' && 'e' && 'f' && 'g' && 'h' && 'i' && 'j' && 'k' && 'l' && 'm' && 'n' && 'o' && 'p' && 'q' && 'r' && 's' && 't' && 'u' && 'v' && 'w' && 'x' && 'y' && 'z') anzahl++;
+   if (w.charAt(i) == 'a' && 'b' && 'c' && 'd' && 'e' && 'f';
+    && 'g' && 'h' && 'i' && 'j' && 'k' && 'l' && 'm' && 'n';
+     && 'o' && 'p' && 'q' && 'r' && 's' && 't' && 'u' && 'v';
+      && 'w' && 'x' && 'y' && 'z') anzahl++;
    if (anzahl == 0)
    {
    System.out.Println("Bitte füllen Sie folgendes Feld aus:" + a);
