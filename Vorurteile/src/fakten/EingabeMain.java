e503fb0a3ebd920bd7cae1d81c900ac35cf582d9
schoebel.maximilian <schoebel.maximilian@PC232621.osz.intern>
2018-01-31 12:56:11 +0100

    Design und Funktionalität der Benutzeroberfläche.

--- Vorurteile/src/fakten/EingabeMain.java
+++ Vorurteile/src/fakten/EingabeMain.java
@@ -2,20 +2,27 @@ package fakten;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 
-public class EingabeMain extends Application {
+public class EingabeMain extends Application
+  {
+
 	@Override
-	public void start(Stage primaryStage) {
-		try {
-			Scene scene = new Scene(FXMLLoader.load(getClass().getResource("EingabeFaktView.fxml")));
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
+	public void start(Stage pGrundStage)
+	 {
+		 try
+		 {
+			 AnchorPane lGrundPane = (AnchorPane)FXMLLoader.load(getClass().getResource("EingabeFaktView.fxml"));
+			 Scene lScene = new Scene(lGrundPane,600,600);
+			 pGrundStage.setScene(lScene);
+			 pGrundStage.show();
+		 }
+		 catch(Exception e) {e.printStackTrace();}
+
+
+  }
 
 	public static void main(String[] args) {
 		launch(args);
