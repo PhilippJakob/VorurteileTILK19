941b81995fee128566d79625c886cac0b2287053
pohle.fin-michael <pohle.fin-michael@PC232627.osz.intern>
2018-01-31 13:08:49 +0100

    fehler geändert

--- Vorurteile/src/fakten/Fakt.java
+++ Vorurteile/src/fakten/Fakt.java
@@ -14,10 +14,10 @@ public class Fakt
     private String quellenTyp;
     private String autor;
     private String link;
-    private LocalDate datum;
-    private LocalDate zeitStempel;
+    private String datum;
+    private String zeitStempel;
 
-	public Fakt(String pTitel, String pAutor, LocalDate pDatum, String pQuellenTyp, String pLink, LocalDate pZeitStempel, IDFakten iDFakten)
+	public Fakt(String pTitel, String pAutor, String pDatum, String pQuellenTyp, String pLink, String pZeitStempel, IDFakten iDFakten)
 	{
 			this.titel = pTitel;
 			this.iDFakten = new IDFakten();
