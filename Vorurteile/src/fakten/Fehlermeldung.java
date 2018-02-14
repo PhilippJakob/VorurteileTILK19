b06da5c61a01b3018b711e04c8c836312ec5d3e1
shahmirzadi.amir_sas <shahmirzadi.amir_sas@PC232629.osz.intern>
2018-02-14 13:09:02 +0100

    "hallo wie geht es" wurde entfernt

--- Vorurteile/src/fakten/Fehlermeldung.java
+++ Vorurteile/src/fakten/Fehlermeldung.java
@@ -25,7 +25,6 @@ package fakten;
  *
  * Feindetail: bei zu wenigen bzw zu vielen Wörtern bei Inhalt/Vorurteil, wird eine Meldung angezeigt
  */
-//hallo wie geht es
 
 /*
  * Feld/er nicht ausgefüllt (beispiel):
