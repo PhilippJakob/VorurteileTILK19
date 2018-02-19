0c22e865cfbe375c1ab24ce0c9d026677896153e
Mielenz.Chantal <Mielenz.Chantal@PC230107.osz.intern>
2018-02-19 10:31:51 +0100

    Auslesemethoden überarbeitet

--- Vorurteile/src/test/Verknüpfung.java
+++ Vorurteile/src/test/Verknüpfung.java
@@ -6,7 +6,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import datenbank.Datenbankverbindung;
-import vorurteile.Vorurteile;
 
 public class Verknüpfung
 {
@@ -16,7 +15,7 @@ public class Verknüpfung
 		{
 			Connection lConnection = Datenbankverbindung.getConnection();
 			Statement lBefehl = lConnection.createStatement();
-			ResultSet lResult = lBefehl.executeQuery("SELECT vu.Titel FROM dbo_vorurteile.vorurteile vo, dbo_vorurteile.vorurteile vu, dbo_vorurteile.verbindung_v_v vv WHERE vv.ID_Untergeordnetes_Vorurteil = vu.ID_Vorurteile AND vv.ID_Vorurteil = vo.ID_Vorurteile AND vo.ID_Vorurteile=" + pVorurteilID + ";");
+			ResultSet lResult = lBefehl.executeQuery("SELECT vu.Titel, vu.ID_Vorurteile FROM dbo_vorurteile.vorurteile vo, dbo_vorurteile.vorurteile vu, dbo_vorurteile.verbindung_v_v vv WHERE vv.ID_Untergeordnetes_Vorurteil = vu.ID_Vorurteile AND vv.ID_Vorurteil = vo.ID_Vorurteile AND vo.ID_Vorurteile=" + pVorurteilID + ";");
 			while(lResult.next())
 			{
 				lListeVorurteile.add(lResult.getString("Titel"));
@@ -40,7 +39,7 @@ public class Verknüpfung
 		{
 			Connection lConnection = Datenbankverbindung.getConnection();
 			Statement lBefehl = lConnection.createStatement();
-			ResultSet lResult = lBefehl.executeQuery("SELECT vo.Titel FROM dbo_vorurteile.vorurteile vo, dbo_vorurteile.vorurteile vu, dbo_vorurteile.verbindung_v_v vv WHERE vv.ID_Untergeordnetes_Vorurteil = vu.ID_Vorurteile AND vv.ID_Vorurteil = vo.ID_Vorurteile AND vu.ID_Vorurteile=" + pVorurteilID + ";");
+			ResultSet lResult = lBefehl.executeQuery("SELECT vo.Titel, vo.ID_Vorurteile FROM dbo_vorurteile.vorurteile vo, dbo_vorurteile.vorurteile vu, dbo_vorurteile.verbindung_v_v vv WHERE vv.ID_Untergeordnetes_Vorurteil = vu.ID_Vorurteile AND vv.ID_Vorurteil = vo.ID_Vorurteile AND vu.ID_Vorurteile=" + pVorurteilID + ";");
 			while(lResult.next())
 			{
 				lListeVorurteile.add(lResult.getString("Titel"));
@@ -64,7 +63,7 @@ public class Verknüpfung
 		{
 			Connection lConnection = Datenbankverbindung.getConnection();
 			Statement lBefehl = lConnection.createStatement();
-			ResultSet lResult = lBefehl.executeQuery("SELECT f.Titel FROM dbo_vorurteile.fakten f, dbo_vorurteile.verbindung ve, dbo_vorurteile.vorurteile vo WHERE vo.ID_Vorurteile = ve.ID_Vorurteile AND ve.ID_Fakten = f.ID_Fakten AND vo.ID_Vorurteile=" + pVorurteilID + ";");
+			ResultSet lResult = lBefehl.executeQuery("SELECT f.Titel, f.ID_Fakten FROM dbo_vorurteile.fakten f, dbo_vorurteile.verbindung ve, dbo_vorurteile.vorurteile vo WHERE vo.ID_Vorurteile = ve.ID_Vorurteile AND ve.ID_Fakten = f.ID_Fakten AND vo.ID_Vorurteile=" + pVorurteilID + ";");
 			while(lResult.next())
 			{
 				lListeFakten.add(lResult.getString("Titel"));
@@ -80,6 +79,29 @@ public class Verknüpfung
 		}
 		return lListeFakten;
 
+	}
+
+	public static ArrayList<String> suchenVorurteileMitFakten(int pFaktenID){
+		ArrayList<String> lListeFakten = new ArrayList<>();
+		try
+		{
+			Connection lConnection = Datenbankverbindung.getConnection();
+			Statement lBefehl = lConnection.createStatement();
+			ResultSet lResult = lBefehl.executeQuery("SELECT vo.Titel, vo.ID_Vorurteile FROM dbo_vorurteile.fakten f, dbo_vorurteile.verbindung ve, dbo_vorurteile.vorurteile vo WHERE vo.ID_Vorurteile = ve.ID_Vorurteile AND ve.ID_Fakten = f.ID_Fakten AND f.ID_Fakten=" + pFaktenID + ";");
+			while(lResult.next())
+			{
+				lListeFakten.add(lResult.getString("Titel"));
+			}
+		}
+		catch(SQLException ex)
+		{
+			ex.printStackTrace();
+		}
+
+		for(String s : lListeFakten){
+			System.out.println(s);
+		}
+		return lListeFakten;
 	}
 
 }
