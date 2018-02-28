ecdd6aac448ac0ff9a311c938b4b2debc17281d6
fliskowski.nico <fliskowski.nico@PC210806.osz.intern>
2018-02-28 14:38:17 +0100

    Namenskonventionskorrigierung

--- Vorurteile/src/vorurteile/ui/EingabeVorurteilController.java
+++ Vorurteile/src/vorurteile/ui/EingabeVorurteilController.java
@@ -16,181 +16,173 @@ import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import vorurteile.items.Vorurteil;
 
-public class EingabeVorurteilController{
+public class EingabeVorurteilController
+{
+	@FXML
+	private TextField tfTitel;
+	
+	@FXML
+   private TextArea taHauptaussage;
+	
+	@FXML
+   private Button btSpeichernVorurteil;
+	
+	@FXML
+   private TextField tfFaktensuche;
+	
+	@FXML
+   private Button btFaktensuche;
+	
+	@FXML
+   private Button btFaktHinzufügen;
+	
+	@FXML
+   private TableView<Vorurteil> tvFaktenliste;
+	
+	@FXML
+   private TableColumn<Vorurteil, String> tcTitelFaktenliste;
+	
+	@FXML
+   private Button btFaktAuswählen;
+	
+	@FXML
+   private Button btFaktNichtAuswählen;
+	
+	@FXML
+   private Button btRefreshF;
+	
+	@FXML
+   private TableView<Vorurteil> tvFaktenlisteAusgewählt;
+	
+	@FXML
+   private TableColumn<Vorurteil, String> tcTitelFaktenlisteAusgewählt;
+	
+	@FXML
+   private TextField tfVorurteilssuche;
+	
+	@FXML
+   private Button btVorurteilssuche;
+	
+	@FXML
+   private TableView<Vorurteil> tvVorurteilliste;
+	
+	@FXML
+   private TableColumn<Vorurteil, String> tcTitelVorurteilsliste;
+	
+	@FXML
+   private Button btVorurteilAuswählen;
+	
+	@FXML
+   private Button btVorurteilNichtAuswählen;
+	
+	@FXML
+   private Button btRefreshV;
+	
+	@FXML
+   private TableView<Vorurteil> tvVorurteillisteAusgewählt;
+	
+	@FXML
+   private TableColumn<Vorurteil, String> tcTitelVorurteilslisteAusgewählt;
+	
+	@FXML
+   private Label lbErrorF;
+	
+	@FXML
+   private Label lbErrorV;
+	
+	@FXML
+   private TabPane tpFaktVorurteil;
 
-	    @FXML
-	    private TextField tfFaktensuche;
+	@FXML
+	void suchenFakt(ActionEvent event)
+	{
 
-	    @FXML
-	    private TableColumn<?, ?> tcTitelFaktenlisteAusgewählt;
+	};
 
-	    @FXML
-	    private TextField tfTitel;
+	@FXML
+	void auswählenFakt(ActionEvent event)
+	{
 
-	    @FXML
-	    private Button btFaktAuswählen;
+	};
 
-	    @FXML
-	    private TextField tfVorurteilssuche;
+	@FXML
+	void nichtAuswählenFakt(ActionEvent event)
+	{
 
-	    @FXML
-	    private TableView<Vorurteil> tvVorurteilliste;
+	};
 
-	    @FXML
-	    private Button btRefresh;
+	@FXML
+	void neuladenFensterF(ActionEvent event)
+	{
 
-	    @FXML
-	    private TabPane tpFaktVorurteil;
+	};
 
-	    @FXML
-	    private Button btVorurteilAuswählen;
+	@FXML
+	void hinzufügenFakt(ActionEvent event)
+	{
 
-	    @FXML
-	    private TableView<?> tvFaktenliste;
+	};
 
-	    @FXML
-	    private Button btSpeichernVorurteil;
+	@FXML
+	void suchenVorurteil(ActionEvent event)
+	{
+		erstellenTabelle();
+		String lTitel = tfVorurteilssuche.getText().trim();
+		ArrayList<Vorurteil> lVorurteil = vorurteile.VorurteilManager.getVorurteile(lTitel);
+		for(Vorurteil l : lVorurteil )
+		{
+			//System.out.println(l.getID() + " " + l.getTitel());
+			tvVorurteilliste.getItems().add(l);
+		};
+	};
 
-	    @FXML
-	    private TableColumn<?, ?> tcIDFaktenliste;
+	private ObservableList<Vorurteil> getVorurteil()
+	{
+		ObservableList<Vorurteil> getVorurteil = FXCollections.observableArrayList();
+		return getVorurteil;
+	};
 
-	    @FXML
-	    private TextArea taHauptaussage;
+	public void erstellenTabelle()
+	{
+		tcTitelVorurteilsliste.setCellValueFactory(new PropertyValueFactory<>("titel"));
+		tvVorurteilliste.setItems(getVorurteil());
 
-	    @FXML
-	    private Button btFaktensuche;
+	};
 
-	    @FXML
-	    private TableView<?> tvVorurteillisteAusgewählt;
+	@FXML
+	void auswählenVorurteil(ActionEvent event)
+	{
 
-	    @FXML
-	    private TableColumn<?, ?> tcIDFaktenlisteAusgewählt;
+	};
 
-	    @FXML
-	    private Label lbError1;
+	@FXML
+	void nichtAuswählenVorurteil(ActionEvent event)
+	{
 
-	    @FXML
-	    private TableColumn<?, ?> tcIDVorurteilsliste;
+	};
 
-	    @FXML
-	    private TableColumn<?, ?> tcIDVorurteilslisteAusgewählt;
+	@FXML
+	void neuladenFensterV(ActionEvent event)
+	{
 
-	    @FXML
-	    private TableColumn<?, ?> tcTitelVorurteilslisteAusgewählt;
+	};
 
-	    @FXML
-	    private TableColumn<Vorurteil, String> tcTitelVorurteilsliste;
+	@FXML
+	void speichernVorurteil(ActionEvent event)
+	{
+		überprüfenFelder();
+	};
 
-	    @FXML
-	    private Label lbError;
-
-	    @FXML
-	    private Button btVorurteilssuche;
-
-	    @FXML
-	    private Button btRefresh1;
-
-	    @FXML
-	    private Button btFaktHinzufügen;
-
-	    @FXML
-	    private TableColumn<?, ?> tcTitelFaktenliste;
-
-	    @FXML
-	    private Button btVorurteilNichtAuswählen;
-
-	    @FXML
-	    private TableView<?> tvFaktenlisteAusgewählt;
-
-	    @FXML
-	    private Button btFaktNichtAuswählen;
-
-	    @FXML
-	    void suchenFakt(ActionEvent event) {
-
-	    }
-
-	    @FXML
-	    void auswählenFakt(ActionEvent event) {
-
-	    }
-
-	    @FXML
-	    void nichtAuswählenFakt(ActionEvent event) {
-
-	    }
-
-	    @FXML
-	    void neuladenFensterF(ActionEvent event) {
-
-	    }
-
-	    @FXML
-	    void hinzufügenFakt(ActionEvent event) {
-
-	    }
-
-	    @FXML
-	    void suchenVorurteil(ActionEvent event)
-	    {
-	   	erstellenTabelle();
-	   	String lTitel = tfVorurteilssuche.getText().trim();
-	   	ArrayList<Vorurteil> lVorurteil = vorurteile.VorurteilManager.getVorurteile(lTitel);
-	   	for(Vorurteil l : lVorurteil )
-	   	{
-	   		//System.out.println(l.getID() + " " + l.getTitel());
-	   		tvVorurteilliste.getItems().add(l);
-	   	}
-	    }
-
-	    private ObservableList<Vorurteil> getVorurteil()
-	    {
-	   	 ObservableList<Vorurteil> getVorurteil = FXCollections.observableArrayList();
-	   	 return getVorurteil;
-	    }
-
-	    public void erstellenTabelle()
-	    {
-	   	 tcTitelVorurteilsliste.setCellValueFactory(new PropertyValueFactory<>("titel"));
-	   	 tvVorurteilliste.setItems(getVorurteil());
-
-	    }
-
-
-
-	    @FXML
-	    void auswählenVorurteil(ActionEvent event) {
-
-	    }
-
-	    @FXML
-	    void nichtAuswählenVorurteil(ActionEvent event) {
-
-	    }
-
-	    @FXML
-	    void neuladenFensterV(ActionEvent event) {
-
-	    }
-
-	    @FXML
-	    void speichernVorurteil(ActionEvent event) {
-	   	 überprüfenFelder();
-
-	    }
-
-	    private void überprüfenFelder() {
-	   	if (taHauptaussage.getText().isEmpty() || tfTitel.getText().isEmpty())
-	   	{
-	   		lbError.setText("Error: Bitte alle Felder ausfüllen");
-	   		//System.out.println("Error: Bitte die Hauptaussage ausfüllen");
-	   	}
-
-	   	else
-	   	{
-	   		lbError.setText("");
-   	}
-
-	    }
-
-}
+	private void überprüfenFelder()
+	{
+		if (taHauptaussage.getText().isEmpty() || tfTitel.getText().isEmpty())
+		{
+			lbErrorF.setText("Error: Bitte alle Felder ausfüllen");
+			//System.out.println("Error: Bitte die Hauptaussage ausfüllen");
+		}
+		else
+		{
+			lbErrorF.setText("");
+		};
+	};
+};
