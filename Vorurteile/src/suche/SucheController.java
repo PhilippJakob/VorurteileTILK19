01007d8e2777ec4057f0389995258f650b437610
scrade1.jason <scrade1.jason@PC230116.osz.intern>
2018-03-05 11:13:22 +0100

    Suchleiste

---
+++ Vorurteile/src/suche/SucheController.java
@@ -0,0 +1,25 @@
+package suche;
+/*
+ * Erstellt von Chantal Mielenz am 05.03.2018
+ */
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class SucheController {
+
+    @FXML
+    private TextField tfSuchleiste;
+
+    @FXML
+    private Button btSuche;
+
+    @FXML
+    private TableView<?> tvVorurteilsliste;
+
+    @FXML
+    private TableColumn<?, ?> tcVorurteilsliste;
+
+}
---
+++ Vorurteile/src/suche/SucheMain.java
@@ -0,0 +1,22 @@
+package suche;
+/*
+ * Erstellt von Chantal Mielenz am 05.03.2018
+ */
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class SucheMain extends Application {
+	
+	
+	@Override
+	public void start(Stage primaryStage) {
+		
+		
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	
+}
