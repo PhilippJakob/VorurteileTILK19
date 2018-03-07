d8cbff0ed35ff89e8fbf48e1b83974979d9f9a2d
Mielenz.Chantal <Mielenz.Chantal@PC230117.osz.intern>
2018-03-07 12:24:35 +0100

    Suchleisten Oberfläche fertig gestellt

--- Vorurteile/src/suche/SucheMain.java
+++ Vorurteile/src/suche/SucheMain.java
@@ -1,17 +1,24 @@
 package suche;
+import java.io.IOException;
+
 /*
  * Erstellt von Chantal Mielenz am 05.03.2018
  */
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class SucheMain extends Application {
 	
 	
 	@Override
-	public void start(Stage primaryStage) {
-		
-		
+	public void start(Stage pPrimaryStage) throws IOException {
+		Parent lParent = FXMLLoader.load(getClass().getResource("SucheView.fxml"));
+		pPrimaryStage.setScene(new Scene(lParent));
+		pPrimaryStage.setTitle("Suche der Vorurteile");
+		pPrimaryStage.show();
 	}
 
 	public static void main(String[] args) {
