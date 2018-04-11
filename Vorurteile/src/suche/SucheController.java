fd11ff775bc4b78c17e73b0bd7f70cfe18b30d75
henderkes.florian <henderkes.florian@PC230116.osz.intern>
2018-04-11 12:20:11 +0200

    Methode umbenannt

--- Vorurteile/src/suche/SucheController.java
+++ Vorurteile/src/suche/SucheController.java
@@ -1,19 +1,13 @@
 package suche;
+
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 import datenbank.Datenbankverbindung;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
-/** 
-  * Angelegt: 05.03.2018, Chantal Mielenz, Controller für die Suchleiste
-  */
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
@@ -21,12 +15,14 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import vorurteile.items.Vorurteil;
+
 /**
  * Angelegt: 05.03.2018, Chantal Mielenz
  * Geändert: 07.03.2018, Chantal Mielenz, Methode hinzugefügt
  * Geändert: 19.03.2018, Chantal Mielenz, Methode verändert / erweitert
- * Geändert: 21.03.2018, Florian Henderkes & Chantal Mielenz, Methode suchenVorurteile verändert / erweitert
+ * Geändert: 21.03.2018, Florian Henderkes & Chantal Mielenz, Methoden suchenVorurteile und suchenVorurteileNachSuchwörter verändert / erweitert
  */
+
 public class SucheController 
 {
 
@@ -45,10 +41,8 @@ public class SucheController
     @FXML
     void suchenVorurteile(ActionEvent event) 
     {
-   	 	
    	 	if(tfSuchleiste.getText().isEmpty())
-   	 	{	
-   	 		//sinnvolle Fehlermeldung hier hinzufügen 
+   	 	{
    	 		tcVorurteilsliste.setText("Fehler!");
    	 	}
    	 	else
@@ -56,25 +50,16 @@ public class SucheController
    	 		System.out.println(tfSuchleiste.getText());
    	 		String lSuche = tfSuchleiste.getText();
    	 		String[] lSuchwörter = lSuche.split(" ");
-   	 		ObservableList<Vorurteil> lVorurteile = null;
-   	 		
-   	 		
+   	 		ObservableList<Vorurteil> lVorurteile = FXCollections.observableArrayList();
    	 		for(String suchwort : lSuchwörter)
    	 		{
-   	 			lVorurteile = suchenVorurteileNachSuchwörter(suchwort);
-   	 		
+   	 			lVorurteile = suchenVorurteileNachSuchwort(suchwort);
    	 		}
-   	 		
-   	 		tcVorurteilsliste.setCellValueFactory(new PropertyValueFactory<Vorurteil, String>("titel"));
-			
-			for(Vorurteil v : lVorurteile){
-				System.out.println(v.getTitel());
-				
-			}
+   	 		//tcVorurteilsliste.setCellValueFactory(new PropertyValueFactory<Vorurteil, String>("titel"));
    	 	}
     }
     
-    public static ObservableList<Vorurteil> suchenVorurteileNachSuchwörter(String pSuchwörter)
+    public static ObservableList<Vorurteil> suchenVorurteileNachSuchwort(String pSuchwort)
 	{
 		ObservableList<Vorurteil> lListeVorurteile = FXCollections.observableArrayList();
 		try
@@ -82,22 +67,16 @@ public class SucheController
 			Connection lVerbindung = Datenbankverbindung.getConnection();
 			Statement lBefehl = lVerbindung.createStatement();
 			ResultSet lErgebnis = lBefehl.executeQuery
-					("SELECT vu.Titel, vu.ID_Vorurteile, vu.Autor, vu.Internetquelle_Ja_Nein, vu.Link FROM dbo_vorurteile.vorurteile vu WHERE vu.Titel LIKE '%" + pSuchwörter + "%';");
+					("SELECT vu.Titel, vu.ID_Vorurteile, vu.Autor, vu.Internetquelle_Ja_Nein, vu.Link FROM dbo_vorurteile.vorurteile vu WHERE vu.Titel LIKE '%" + pSuchwort + "%';");
 			while(lErgebnis.next())
 			{
 				lListeVorurteile.add(new Vorurteil(lErgebnis.getInt("vu.ID_Vorurteile"), lErgebnis.getString("vu.Titel"), lErgebnis.getString("vu.Autor"), 0, lErgebnis.getString("vu.Internetquelle_Ja_Nein"), lErgebnis.getString("vu.Link")));
-
 			}
 		}
 		catch(SQLException ex)
 		{
 			ex.printStackTrace();
 		}
-		
-		
-
 		return lListeVorurteile;
-
 	}
-    
 }
